d0a935d73e1ff0334cf1e2655b9ffb6143c86b27
hesptech <olea.oswaldo@gmail.com>
2024-08-27 12:50:32 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_navigationbar_31
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_navigationbar_31/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_navigationbar_31
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
